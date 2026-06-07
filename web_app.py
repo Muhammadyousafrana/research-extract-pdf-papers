e3d62fbf8598348d2da126d3d1a6653afa3f336d
Muhammadyousafrana <yousaf.mlengineer@gmail.com>
2026-06-07 05:09:41 -0700

    heroku config added

--- web_app.py
+++ web_app.py
@@ -1465,4 +1465,5 @@ async def admin_cleanup_expired(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
